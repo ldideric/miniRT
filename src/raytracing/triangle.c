df60b0ad94a64c8c5a69fb22b972604a481a1a34
ldideric <lietze14@gmail.com>
2020-06-11 15:47:51 +0200

    update

--- src/raytracing/triangle.c
+++ src/raytracing/triangle.c
@@ -6,7 +6,7 @@
 /*   By: ldideric <ldideric@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/03/25 19:02:31 by ldideric      #+#    #+#                 */
-/*   Updated: 2020/06/10 19:37:06 by ldideric      ########   odam.nl         */
+/*   Updated: 2020/06/10 21:35:48 by ldideric      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,7 @@ static int		tr_right_side(t_vect v0, t_vect v1, t_vect P, t_vect N)
 	edge = vect_min(v1, v0);
 	vp = vect_min(P, v0);
 	C = cross_pr(edge, vp);
-	if (dot(N, C) < 0)
-		return (0);
-	return (1);
+	return (dot(N, C) >= 0);
 }
 
 t_vect			triangle(t_vect rd, t_data *data, t_objs o)
@@ -49,11 +47,9 @@ t_vect			triangle(t_vect rd, t_data *data, t_objs o)
 		return ((t_vect){0, 0, 0, 0});
 	P = vec3(rd.x * t, rd.y * t, rd.z * t);
 	P = vect_plus(data->b.cam.c[data->b.i_c].pos, P);
-	if (tr_right_side(o.pos1, o.pos2, P, N) == 0)
-		return ((t_vect){0, 0, 0, 0});
-	if (tr_right_side(o.pos2, o.pos3, P, N) == 0)
-		return ((t_vect){0, 0, 0, 0});
-	if (tr_right_side(o.pos3, o.pos1, P, N) == 0)
+	if (tr_right_side(o.pos1, o.pos2, P, N) == 0 ||
+		tr_right_side(o.pos2, o.pos3, P, N) == 0 ||
+		tr_right_side(o.pos3, o.pos1, P, N) == 0)
 		return ((t_vect){0, 0, 0, 0});
 	P.hit = 1;
 	printf("[HIT]%f,%f,%f,%d\n", P.x, P.y, P.z, P.hit);

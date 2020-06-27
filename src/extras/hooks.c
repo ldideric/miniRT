87fa76ac76b127d5ac649a1d447add0eb1400ceb
Lietze <lietze14@gmail.com>
2020-06-27 17:19:39 +0200

    cache switch somewhat better

--- src/extras/hooks.c
+++ src/extras/hooks.c
@@ -6,7 +6,7 @@
 /*   By: ldideric <ldideric@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/05 15:21:31 by ldideric      #+#    #+#                 */
-/*   Updated: 2020/06/25 16:22:48 by ldideric      ########   odam.nl         */
+/*   Updated: 2020/06/27 16:30:50 by ldideric      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,30 +35,34 @@ static int			cam_change_hooks(int keycode, t_vars *vars)
 	if (keycode == 124 && vars->data.b.cam.max > 1)
 	{
 		if (vars->data.sav[vars->data.b.i_c] == NULL)
+		{
 			vars->data.sav[vars->data.b.i_c] = vars->data.img;
+			vars->data.img = mlx_new_image(vars->mlx, vars->data.b.res.x_max, vars->data.b.res.y_max);
+			vars->data.addr = mlx_get_data_addr(vars->data.img, &(vars->data.bpp), &(vars->data.len), &(vars->data.endian));
+		}
+		else
+			vars->data.addr = mlx_get_data_addr(vars->data.sav[vars->data.b.i_c], &(vars->data.bpp), &(vars->data.len), &(vars->data.endian));
 		if (vars->data.b.i_c == vars->data.b.cam.max - 1)
 			vars->data.b.i_c = 0;
 		else
 			vars->data.b.i_c++;
 		saved_images_hooks(vars);
-		// ft_printf("Rendering...\n");
-		// px_loop(&vars->data);
-		// ft_printf("Done!\n");
-		// mlx_put_image_to_window(vars->mlx, vars->win, vars->data.img, 0, 0);
 	}
 	else if (keycode == 123 && vars->data.b.cam.max > 1)
 	{
 		if (vars->data.sav[vars->data.b.i_c] == NULL)
+		{
 			vars->data.sav[vars->data.b.i_c] = vars->data.img;
+			vars->data.img = mlx_new_image(vars->mlx, vars->data.b.res.x_max, vars->data.b.res.y_max);
+			vars->data.addr = mlx_get_data_addr(vars->data.img, &(vars->data.bpp), &(vars->data.len), &(vars->data.endian));
+		}
+		else
+			vars->data.addr = mlx_get_data_addr(vars->data.sav[vars->data.b.i_c], &(vars->data.bpp), &(vars->data.len), &(vars->data.endian));
 		if (vars->data.b.i_c == 0)
 			vars->data.b.i_c = vars->data.b.cam.max - 1;
 		else
 			vars->data.b.i_c--;
 		saved_images_hooks(vars);
-		// ft_printf("Rendering...\n");
-		// px_loop(&vars->data);
-		// ft_printf("Done!\n");
-		// mlx_put_image_to_window(vars->mlx, vars->win, vars->data.img, 0, 0);
 	}
 	return (0);
 }
